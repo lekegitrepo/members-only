67abcffb7ec4bb551d392d258232ddfe3b0896cf
Ronald Tingchaleun <ronald.tingchaleun@gmail.com>
2019-11-06 10:57:29 -0500

    Moved sign up and log in links to the application layout, as required by TSE.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
   root 'posts#index'
-
+  get '/user/new', to: 'users#new'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#delete'
-
-  #get 'posts', to: 'posts#index'
-  #post 'sessions/new'
-
   resources :users
   resources :posts
   resources :sessions
